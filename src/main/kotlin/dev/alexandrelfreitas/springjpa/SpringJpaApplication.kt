1a01384bd24883a61528c833c0860d9658ae40ff
Alexandre <allezito@gmail.com>
2019-08-06 20:03:35 -0300

    primeiro commit

---
+++ src/main/kotlin/dev/alexandrelfreitas/springjpa/SpringJpaApplication.kt
@@ -0,0 +1,11 @@
+package dev.alexandrelfreitas.springjpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringJpaApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringJpaApplication>(*args)
+}
